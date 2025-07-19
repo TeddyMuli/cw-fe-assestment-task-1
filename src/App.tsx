a24b94aa1bc2c77c7f445d84bf1b09429a90431f
TeddyMuli <tmuli974@gmail.com>
2025-07-19 21:21:58 +0300

    fix: fix import for HeroSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import BoxArea97 from "./components/HeroSection";
+import HeroSection from "./components/HeroSection";
 import TagList from "./components/TagList";
 
 const tags = [
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <main className="bg-[#121417] min-h-screen text-white">
       <Header />
-      <BoxArea97 />
+      <HeroSection />
       <TagList title="Trending" tags={tags} />
       <TagList title="For you" tags={tags} />
     </main>
