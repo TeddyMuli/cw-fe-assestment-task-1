2b30b98d50374c34c4f807292806ca3d3cfe71b3
TeddyMuli <tmuli974@gmail.com>
2025-07-19 22:27:30 +0300

    modify input component to remove focus ring

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -28,7 +28,7 @@ export default function SearchBar({
         onChange={(e) => setInnerValue(e.target.value)}
         type="text"
         placeholder="Type to search..."
-        className="flex-1 bg-transparent border-none text-white placeholder:text-[#9EABB8] placeholder:text-[16px] focus:ring-0"
+        className="flex-1 bg-transparent border-none text-white placeholder:text-[#9EABB8] placeholder:text-[16px] outline-none"
       />
       <Button className="bg-[#1A80E5] hover:bg-blue-500 text-white ml-4 text-[16px] w-[96px] h-[48px]" >
         Search
