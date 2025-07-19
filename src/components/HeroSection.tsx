84c3a3af973fd59068088a736867d6d2ca186b6b
TeddyMuli <tmuli974@gmail.com>
2025-07-19 22:22:48 +0300

    make width of heros section mobile responsive to as to see margins

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,11 +7,11 @@ export default function HeroSection() {
   };
 
   return (
-    <div className="relative w-full max-w-5xl mx-auto rounded-xl overflow-hidden mt-8">
+    <div className="relative w-[90%] lg:w-full max-w-5xl mx-auto rounded-xl overflow-hidden mt-8">
       <img src="/task1/hero-bg.png" className="w-full h-96 object-cover" />
       <div className="absolute inset-0 bg-black/50 flex flex-col items-center justify-center text-center px-4">
-        <h1 className="text-3xl md:text-5xl font-bold text-white">
-          Search for words, phrases and<br />meanings
+        <h1 className="text-3xl md:text-5xl max-w-[720px] font-bold text-white">
+          Search for words, phrases and meanings
         </h1>
         <SearchBar initialValue="" onSearch={onSearch} />
       </div>
